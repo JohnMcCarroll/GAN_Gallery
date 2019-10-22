d2a81328a54f6e88a14ac75c3be0ca06742f3a55
Troy <troypotter99@gmail.com>
2019-10-22 14:13:27 -0400

    Split string for image url

--- src/wikiAPI.py
+++ src/wikiAPI.py
@@ -16,19 +16,19 @@ artistIDs = set()
 imageURLs = set()
 
 picURL = 'https://uploads1.wikiart.org/images/gerhard-richter/abstract-painting-780-1.jpg!Large.jpg'
-##Folder to save image to
+# Folder to save image to
 picPath = 'images/'
-##Title of Picture
+# Title of Picture
 picTitle = 'Abstract'
 
 
 def getImage(picURL, picPath, picTitle):
     finalPath = picPath + picTitle + '.jpg'
-    ##image = urllib.URLopener()
+    # image = urllib.URLopener()
     urllib.request.urlretrieve(picURL, finalPath)
 
 def getArtists():
-    ## get list of artists by genre / medium and store in set
+    # get list of artists by genre / medium and store in set
     artistsURL = "https://www.wikiart.org/en/api/2/UpdatedArtists"
     paginatedURL = ""
     hasMore = True
@@ -44,6 +44,7 @@ def getArtists():
         if len(request) < 2:
             break
 
+        # Checking hasMore in split string for true or false
         if request[1] == "true}":
             hasMore = True
         else:
@@ -52,7 +53,10 @@ def getArtists():
         # update pagination token
         request = request[0].split("\"paginationToken\":")
         paginationToken = request[1].strip("\",")
+
+        print(request)
         print(paginationToken)
+
 
         # update URL
         paginatedURL = "?paginationToken=" + paginationToken
@@ -65,15 +69,20 @@ def getArtists():
             id = artist[-26:-2]
             artistIDs.add(id)
 
-        #time.sleep(10)
+        # time.sleep(10)
 
-# populates imageURLs set with painting URLs (TODO: change to dict() with title as keys)
+# populates imageURLs set with painting URLs
 def getPaintingURLs():
+    # Main url
     listURL = "https://www.wikiart.org/en/api/2/PaintingsByArtist?id="
     paginatedURL = ""
+    # Set hasMore default to true
     hasMore = True
+    # Set default token to empty string
     paginationToken = ""
     for id in artistIDs:
+
+        # while hasMore = true
         while hasMore:
             # get page of list of artists
             request = requests.get(listURL + id + paginatedURL).text
@@ -97,23 +106,28 @@ def getPaintingURLs():
             # update URL
             paginatedURL = "&paginationToken=" + paginationToken
 
+
             # add to set of URLs
             request = request[0].split("\"height\":")
             for artist in request:
                 artist = artist.split("\"image\":")
                 if artist[-2] == "]":
                     continue
-                #id = artist[-26:-2]
+                # id = artist[-26:-2]
                 imageURLs.add(id)
 
 
 
-    ## use pagination token to get each new page and use hasMore boolean to know when to move on
+            # get Image URL - "image":"https://uploads.wikiart.org/Content/images/ARTIST-480x600.jpg","wikipediaUrl":
+            # This should split a string twice to single out the image url
+            imageSplit = request.split('\"image\":\"')
+            linkSplit = imageSplit.split('\",\"wikipediaUrl\"')
 
-    ## iterate through set using artist IDs to get list of painting IDs
-    ## use pagination token to get each new page and use hasMore boolean to know when to move on
+            imageURLs.add(linkSplit[0])
 
-    ## download each painting in painting ID set in intervals of 10 seconds?
+            # loop through imageUrls and download each image using function above
+
+
 
 
 getArtists()
