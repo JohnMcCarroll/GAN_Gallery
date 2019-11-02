002b4642819ded1c497066dcc0966644620506b4
John McCarroll <jtm5356@rit.edu>
2019-11-02 14:31:38 -0400

    "fixed Generator class to output 400x400 images with only 3 transposed convolutional layers"

--- src/Generator.py
+++ src/Generator.py
@@ -14,26 +14,14 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         # set up layers as class attributes
-        self.convTrans1 = nn.ConvTranspose2d(125, 100, 4, 2)        #params(input channels, output channels, kernel size, input padding)
-        self.batchNorm1 = nn.BatchNorm2d(100)                       #normalizing the outputs of the first layer
+        self.convTrans1 = nn.ConvTranspose2d(125, 50, 31, 3)        #params(input channels, output channels, kernel size, input padding)
+        self.batchNorm1 = nn.BatchNorm2d(50)                       #normalizing the outputs of the first layer
 
-        self.convTrans2 = nn.ConvTranspose2d(100, 80, 4, 2)
-        self.batchNorm2 = nn.BatchNorm2d(80)
+        self.convTrans2 = nn.ConvTranspose2d(50, 10, 61, 3)
+        self.batchNorm2 = nn.BatchNorm2d(10)
 
-        self.convTrans3 = nn.ConvTranspose2d(80, 50, 8, 2)
-        self.batchNorm3 = nn.BatchNorm2d(50)
-
-        self.convTrans4 = nn.ConvTranspose2d(50, 30, 6, 2)
-        self.batchNorm4 = nn.BatchNorm2d(30)
-
-        self.convTrans5 = nn.ConvTranspose2d(30, 10, 10, 2)
-        self.batchNorm5 = nn.BatchNorm2d(10)
-
-        self.convTrans6 = nn.ConvTranspose2d(10, 5, 8, 2)
-        self.batchNorm6 = nn.BatchNorm2d(5)
-
-        self.convTrans7 = nn.ConvTranspose2d(5, 3, 10, 2)
-        self.batchNorm7 = nn.BatchNorm2d(3)
+        self.convTrans3 = nn.ConvTranspose2d(10, 3, 100, 2)
+        self.batchNorm3 = nn.BatchNorm2d(3)
 
         self.ReLU = nn.ReLU()                                   #Rectified Linear Unit to keep our outputs between 0 and 1
 
@@ -49,23 +37,6 @@ class Generator(nn.Module):
         t = self.convTrans3(t)
         t = self.batchNorm3(t)
         t = self.ReLU(t)
-
-        t = self.convTrans4(t)
-        t = self.batchNorm4(t)
-        t = self.ReLU(t)
-
-        t = self.convTrans5(t)
-        t = self.batchNorm5(t)
-        t = self.ReLU(t)
-
-        t = self.convTrans6(t)
-        t = self.batchNorm6(t)
-        t = self.ReLU(t)
-
-        t = self.convTrans7(t)
-        t = self.batchNorm7(t)
-        t = self.ReLU(t)
-
         return t
 
 gen = Generator()
