92be7e9c3d2cfb0ffe8944329d4bdb66afb18b37
John McCarroll <jtm5356@rit.edu>
2019-11-08 17:09:37 -0500

    "write initial version of trainingloop and identified some bugs"

--- src/InputFormatting.py
+++ src/InputFormatting.py
@@ -4,23 +4,45 @@ import os
 from PIL import Image
 import pickle
 
-rootPath = r'D:\GAN_Gallery\resize'
-dataset = list()
+def formatData():
+    rootPath = r'D:\GAN_Gallery\resize'
+    dataset = list()
+    trans = torchvision.transforms.ToTensor()
+    counter = 0
+
+    for img in os.listdir(rootPath):
+        counter += 1
+        if counter < 8600:
+            continue
+
+        try:
+            image = Image.open(rootPath + '\\' + img)
+            image = trans(image)
+            dataset.append(image)
+        except Exception as e:
+            print(str(e))
+
+    with open(r'D:\GAN_Gallery\src\dataset3.db', 'wb') as file:
+        pickle.dump(dataset, file)
 
 
-for img in os.listdir(rootPath):
-    try:
-        image = Image.open(rootPath + '\\' + img)
-        #image = torchvision.transforms.ToPILImage(image)
-        trans = torchvision.transforms.ToTensor()
-        image = trans(image)
-        dataset.append(image)
-    except:
-        print('img doesnt exist')
-        continue
 
-with open(r'D:\GAN_Gallery\src\dataset', 'wb') as file:
+with open(r'D:\GAN_Gallery\src\dataset1.db', 'rb') as file:
+    #pickle.dump(dataset, file)
+    dataset1 = pickle.load(file)
+
+with open(r'D:\GAN_Gallery\src\dataset2.db', 'rb') as file:
+    #pickle.dump(dataset, file)
+    dataset2 = pickle.load(file)
+
+dataset = dataset1 + dataset2
+
+with open(r'D:\GAN_Gallery\src\dataset.db', 'wb') as file:
     pickle.dump(dataset, file)
 
+
 print(len(dataset))
+print("size:")
 print(dataset[0].size())
+print("sample:")
+print(dataset[0])
--- src/TrainingLoop.py
+++ src/TrainingLoop.py
@@ -1,41 +1,64 @@
 import torch
+import torch.optim as optim
 import src.Generator as G
 import src.Discriminator as D
+import pickle
+import torchvision.transforms as trans
+from PIL import Image
 
 # Create/ load networks
 G = G.Generator()
 D = D.Discriminator()
 
 ### load in data
+with open(r'D:\GAN_Gallery\src\dataset1.db', 'rb') as file:
+    dataset1 = pickle.load(file)
 
+real_data = torch.utils.data.DataLoader(dataset1, 10)
 
 ### set up optimizer functions
-# G_solver = optim.RMSprop(G.parameters(), lr=5e-5)
-# D_solver = optim.RMSprop(D.parameters(), lr=5e-5)
+G_solver = optim.RMSprop(G.parameters(), lr=5e-5)
+D_solver = optim.RMSprop(D.parameters(), lr=5e-5)
+
+### Set up hyperparameters
+epoch = 1
 
 ### Training Loop
+for epoch in range(epoch):
 
-    ### Generate bathc of G_sample
-    # input = random 125 vector(s)
-    # G_sample = G(input)
+    for real_sample in real_data:
 
-    ### train discriminator more than generator?
+        ### Generate bathc of G_sample
+        input = [torch.rand(125), torch.rand(125), torch.rand(125), torch.rand(125), torch.rand(125)]    # maybe want more rand inputs for more G samples
+        G_sample = G(input)         # cant use list??***
+
+        ### print generator image
+        img = trans.ToPILImage(G_sample[0])
+        img.show()
+
+        ### train discriminator more than generator?
 
         ### forward feed D
-        # D_real = D(sample)
-        # D_fake = D(G_sample)
+        D_real = D(real_sample)
+        D_fake = D(G_sample)
 
         ### calculate loss function
-        # D_loss = -(torch.mean(D_real) - torch.mean(D_fake))
-        # G_loss = -torch.mean(D_fake)
+        D_loss = -(torch.mean(D_real) - torch.mean(D_fake))             # implementation of wasserstein metric
+        G_loss = -torch.mean(D_fake)
 
         ### train and clipping discriminator weights
-        # D_loss.backward()
-        # D_solver.step()
-        #
-        # for p in D.parameters():
-        #     p.data.clamp_(-0.01, 0.01)
+        D_loss.backward()
+        D_solver.step()
 
-    ### train generator
-    # G_loss.backward()
-    # G_solver.step()
+        for p in D.parameters():
+            p.data.clamp_(-0.01, 0.01)
+
+        ### train generator
+        G_loss.backward()
+        G_solver.step()
+
+with open(r'D:\GAN_Gallery\src\D.cnn', 'wb') as file:
+    pickle.dump(D, file)
+
+with open(r'D:\GAN_Gallery\src\G.cnn', 'wb') as file:
+    pickle.dump(G, file)
